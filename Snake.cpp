b8c27b473cfd2357d8dc55162a811867eda461b0
HaMinhPhung <minhphung6104@gmail.com>
2025-11-30 21:20:11 +0700

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -7,6 +7,12 @@ void gotoxy( int column, int line );
 struct Point{
     int x,y;
 };
+
+const int MAXX = 38;
+const int MAXY = 12;
+const int MINX = 1;
+const int MINY = 1;
+
 class CONRAN{
 public:
     struct Point A[100];
@@ -32,8 +38,23 @@ public:
         if (Huong==3) A[0].y = A[0].y - 1;
 
     }
+    bool KiemTraVaCham(){
+        return (A[0].x<=0 || A[0].x>=MAXX-1 ||
+                A[0].y<=0 || A[0].y>=MAXY-1);
+    }
+    void VeKhung(){
+        for (int i = 0; i < MAXX; i++)
+        {
+            gotoxy(i,0); cout<<"#";
+            gotoxy(i,MAXY-1); cout<<"#";
+        }
+        for (int i = 0; i < MAXY; i++)
+        {
+            gotoxy(0,i); cout<<"#";
+            gotoxy(MAXX-1,i); cout<<"#";
+        }
+    };
 };
-
 int main()
 {
     CONRAN r;
@@ -50,10 +71,16 @@ int main()
         }
         system("cls");
         r.Ve();
+        r.VeKhung();
         r.DiChuyen(Huong);
         Sleep(300);
+    if (r.KiemTraVaCham())
+    {
+        gotoxy(MAXX/2-5,MAXY/2);
+        cout<<"GAME OVER";
+        break;
     }
-
+}
     return 0;
 }
 
